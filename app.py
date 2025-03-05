4a18b8c020ea2fcdad74aad24dd9374b3853d7cd
abhishekbansode-git <abhishekbansode0774@gmail.com>
2025-03-05 20:15:35 +0530

    Fixed app.py

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def predict():
         return jsonify({'error': str(e), 'trace': traceback.format_exc()})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get('PORT', 10000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port, debug=True)
